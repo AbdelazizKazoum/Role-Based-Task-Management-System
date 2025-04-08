306b455583f87c3afbede9de50de7c63da3b3e77
Abdelaziz kazoum <abdukazoum@gmail.com>
2025-04-08 17:27:27 +0200

    install pino prettier and use validation pipes

--- backend/src/modules/activity-logs/activity-logs.module.ts
+++ backend/src/modules/activity-logs/activity-logs.module.ts
@@ -2,8 +2,24 @@
 import { Module } from '@nestjs/common';
 import { ActivityLogsService } from './activity-logs.service';
 import { ActivityLogsController } from './activity-logs.controller';
+import { DatabaseModule } from 'src/common/database/database.module';
+import {
+  ActivityLogDocument,
+  ActivityLogSchema,
+} from './entities/activity-log.schema';
+import { LoggerModule } from 'src/common/logger/logger.module';
 
 @Module({
+  imports: [
+    DatabaseModule,
+    DatabaseModule.forFeature([
+      {
+        name: ActivityLogDocument.name,
+        schema: ActivityLogSchema,
+      },
+    ]),
+    LoggerModule,
+  ],
   controllers: [ActivityLogsController],
   providers: [ActivityLogsService],
 })
